d3354001efc891b5edde24d678044f633d7553ae
Mateus Silva <mateuss_gt@hotmail.com>
2026-02-23 13:16:35 -0300

    fix: card padding

--- lib/features/home/presentation/widgets/dashboard_view.dart
+++ lib/features/home/presentation/widgets/dashboard_view.dart
@@ -85,7 +85,7 @@ class DashboardView extends StatelessWidget {
                 child: ConstrainedBox(
                   constraints: const BoxConstraints(maxWidth: 800),
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    padding: const EdgeInsets.all(16.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
